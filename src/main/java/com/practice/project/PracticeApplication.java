9868d95ef53b1e7876d6fca45a89925ce1922cb4
Machio1 <bhaktadev@hotmail.com>
2026-08-31 16:24:34 +0200

    Log greeting

--- src/main/java/com/practice/project/PracticeApplication.java
+++ src/main/java/com/practice/project/PracticeApplication.java
@@ -3,14 +3,15 @@ package com.practice.project;
 import com.practice.project.services.PracticeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class PracticeApplication {
 
     public static void main(String[] args) {
         PracticeService practiceService = new PracticeService();
-        String greeting = practiceService.getGreeting();
-        System.out.println(greeting);
+        Logger logger = Logger.getLogger(PracticeApplication.class.getName());
+        logger.info(practiceService.getGreeting());
         SpringApplication.run(PracticeApplication.class, args);
     }
 
